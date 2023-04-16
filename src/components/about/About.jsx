b2be02cb041c69a2dac85481b6f30a6395648591
shaghayegh rabiee <rabiee.sh65@gmail.com>
2023-04-16 13:01:24 +0200

    about-section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,15 +1,47 @@
 import React from "react";
-import myPic from "../../asset/3.jpg";
+import myPic from "../../asset/2.jpg";
+import { BsAward } from "react-icons/bs";
+import { AiFillFolderOpen } from "react-icons/ai";
 import "./about.css";
 
 const About = () => {
   return (
     <section id="about" className="about-section">
-      <div className="myPic">
-        <img src={myPic} alt="about-myPic" className="image-about" />
-      </div>
-      <div>
-        <div className="about-card"></div>
+      <h5>Get to know</h5>
+      <h2>About me</h2>
+      <div className="container about_container">
+        <div className="about_img">
+          <div>
+            <img src={myPic} alt="about-myPic" className="about_img_myPic" />
+          </div>
+        </div>
+        <div>
+          <div className="about-cards">
+            <article className="about-card">
+              <BsAward className="about_icon" />
+              <h5>Experience</h5>
+              <small>+6 years</small>
+            </article>
+            <article className="about-card">
+              <AiFillFolderOpen className="about_icon" />
+              <h5>Projects</h5>
+              <small>In backend and frontend</small>
+            </article>
+          </div>
+          <div className="about_content">
+            <p>
+              Passionate, driven, and competitive front-end developer with more
+              than 6 years of experience as a software engineer with a
+              demonstrated history of working in the information technology and
+              services industry. Skilled in markup languages, e.g., Html, CSS,
+              and scripting languages such as JavaScript and related frameworks
+              like Bootstrap. Experienced proactively in React JS.
+            </p>
+            <a href="#contact" className="btn btn-primary">
+              Let's talk
+            </a>
+          </div>
+        </div>
       </div>
     </section>
   );
